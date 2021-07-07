7dbbf3c4d86770e1bb60a56a8b7dd383dd75a5f7
Harsh Joshi <harshjoshi989@gmail.com>
2021-07-07 20:29:54 +0530

    Remove accent

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,6 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        accentColor: Colors.amberAccent,
         appBarTheme: AppBarTheme()
       ),
 
