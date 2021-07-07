22fbc1fb23d766fac2ddee9ecb60167742a26e3e
Harsh Joshi <harshjoshi989@gmail.com>
2021-07-07 20:25:07 +0530

    Accent color added

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        accentColor: Colors.amberAccent,
         appBarTheme: AppBarTheme()
       ),
 
